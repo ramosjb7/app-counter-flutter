f3a1d43321664b57ccaa7175bb9f81cc9dce9a61
brayan <br4y47.r4m0s@gmail.com>
2022-08-15 16:47:24 -0400

    Comienzo de la app counter

---
+++ lib/screens/counter_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class CounterScreen extends StatefulWidget {
+  const CounterScreen({ Key? key }) : super(key: key);
+
+  @override
+  State<CounterScreen> createState() => _CounterScreenState();
+}
+
+class _CounterScreenState extends State<CounterScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
